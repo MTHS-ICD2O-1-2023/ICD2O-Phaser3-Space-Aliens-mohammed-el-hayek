f09c28c7003819ae680250b300d59e4c605779eb
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-04-16 03:24:00 +0000

    V1.2

---
+++ js/game.js
@@ -0,0 +1,28 @@
+//Created by: Mohammed EL-hayek
+//Created on: Apr 2024
+//This is the Phaser3 game configuration file
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug : true,
+    },
+  },
+  //set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
